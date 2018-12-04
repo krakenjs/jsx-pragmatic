d0edd69aa536860afa5b213d388a0d1cede306a2
Daniel Brain <daniel@bluesuncorp.co.uk>
2018-12-04 13:34:29 -0800

    Allow event-handler to be undefined

--- src/renderers/dom.js
+++ src/renderers/dom.js
@@ -152,7 +152,7 @@ function addProps({ el, props } : AddPropsOptions) {
         }
 
         if (DOM_EVENT.hasOwnProperty(prop)) {
-            if (typeof val !== 'function') {
+            if (typeof val !== 'undefined' && typeof val !== 'function') {
                 throw new TypeError(`Prop ${ prop } must be function`);
             }
 
