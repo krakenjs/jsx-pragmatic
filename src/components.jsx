17c7d89eaadf45758460a84f2c87a26659b6fc95
Daniel Brain <dbrain@paypal.com>
2020-04-27 17:16:32 -0700

    Allow children to be passed into Style tag

--- src/components.jsx
+++ src/components.jsx
@@ -1,14 +1,17 @@
 /* @flow */
 /** @jsx node */
 
-import { node, type ChildType } from './node';
+import { Fragment, node, type ChildType, type ChildrenType } from './node';
 
 type StyleProps = {|
     css : string | {| _getCss : () => string |}
 |};
 
-export function Style({ css } : StyleProps) : ChildType {
+export function Style({ css } : StyleProps, children? : ChildrenType) : ChildType {
     return (
-        <style innerHTML={ typeof css === 'string' ? css : css._getCss() } />
+        <Fragment>
+            <style innerHTML={ typeof css === 'string' ? css : css._getCss() } />
+            { children }
+        </Fragment>
     );
 }
