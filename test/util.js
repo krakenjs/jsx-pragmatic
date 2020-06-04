d7ca1bdee4ff98c89cc241552c1a5f9980511070
Daniel Brain <daniel@brainblocks.io>
2020-06-04 14:30:34 -0700

    Lint fix

--- test/util.js
+++ test/util.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 window.console.karma = function consoleKarma() {
+    // eslint-disable-next-line compat/compat
     const karma = window.karma || (window.top && window.top.karma) || (window.opener && window.opener.karma);
     karma.log('debug', arguments);
     console.log.apply(console, arguments); // eslint-disable-line no-console
