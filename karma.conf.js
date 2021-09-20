1f4f5c5065827a13ee66007b52e4b2895e7ea503
jadutter <4691511+jadutter@users.noreply.github.com>
2021-09-20 13:31:01 -0400

    lint

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ import { getKarmaConfig } from 'grumbler-scripts/config/karma.conf';
 
 import { WEBPACK_CONFIG_TEST } from './webpack.config';
 
-export default function configKarma(karma: Object) {
+export default function configKarma(karma : Object) {
     const karmaConfig = getKarmaConfig(karma, {
         basePath: __dirname,
         webpack:  WEBPACK_CONFIG_TEST
